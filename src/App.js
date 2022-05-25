e2d25e725c93a383fc1ff76fab7e3f7e59ec8687
baabyygalll <andiraazzahra19@gmail.com>
2022-05-25 12:33:54 +0700

    add task section 17

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import NameForm from "./components/NameForm"
+import Search from './components/Search';
+
+function App() {
+  return (
+    <div>Testing</div>
+  );
+}
+
+export default App;
